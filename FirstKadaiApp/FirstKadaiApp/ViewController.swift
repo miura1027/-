4aac4fee80a105f721a481ec0a96fd688d219932
miura1027 <lasukaru1027@icloud.com>
2020-08-30 18:40:32 +0900

    はじめてのアプリのラベルを追加した

---
+++ FirstKadaiApp/FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 三浦大輝 on 2020/08/30.
+//  Copyright © 2020 三浦大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
